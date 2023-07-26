ddf29f42aad213d1fdc60e1f542f8d0800b14306
vijaybhaskar-123 <vijay@solulab.com>
2023-07-26 09:07:55 +0530

    third commit

--- src/CreateExpence/CreateExpence.js
+++ src/CreateExpence/CreateExpence.js
@@ -1,6 +1,61 @@
 import React from 'react'
 import '../CreateExpence/CreateExpence.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const CreateExpence = () => {
+  const naviagte=useNavigate()
+  const [nameValue,setName]=useState('')
+  const [desciptionValue,setdesciption]=useState('')
+  const [categoryValue,seCategory]=useState('')
+  const [dateOfExpenceValue,setdateOfExpence]=useState('')
+  const [expenenceAmoutValue,setexpenenceAmout]=useState('')
+
+  const nameHandler=(e)=>{
+    setName(e.target.value)
+  }
+  const descriptionHandler=(e)=>{
+    setdesciption(e.target.value)
+  }
+  const categoryHandler=(e)=>{
+    seCategory(e.target.value)
+  }
+  const dateOfExpenceHandler=(e)=>{
+    setdateOfExpence(e.target.value)
+  }
+  const expenenceAmoutHandler=(e)=>{
+    setexpenenceAmout(e.target.value)
+  }
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    if(nameValue==="" || desciptionValue==="" ||categoryValue===""||dateOfExpenceValue===""||expenenceAmoutValue===""){
+      alert('Please Fill details')
+    }
+    else{
+      const createExpenceData={
+        nameValue:nameValue,
+        desciptionValue:desciptionValue,
+        categoryValue:categoryValue,
+        dateOfExpenceValue:dateOfExpenceValue,
+        expenenceAmoutValue:expenenceAmoutValue
+      }
+      const storedData= JSON.parse(localStorage.getItem("createExpenceData"))|| []
+      storedData.push(createExpenceData)
+      localStorage.setItem('createExpenceData',JSON.stringify(storedData))
+
+      naviagte('/ViewExpence')
+    }
+  
+   setName('')
+   setdesciption('')
+   seCategory('')
+   setdateOfExpence('')
+   setexpenenceAmout('')
+   
+  }
+ const NaviagetToExpence =()=>{
+  naviagte('/ViewExpence')
+ }
   return (
     <div >
       <div>
@@ -9,11 +64,11 @@ const CreateExpence = () => {
       <div>
         <form className='create-formExpence-container'>
           <label>Name</label>
-          <input type="text" placeholder='Name of The Expence' />
+          <input type="text" placeholder='Name of The Expence' name='name' value={nameValue}  onChange={nameHandler}/>
           <label>Description</label>
-          <input type='text' placeholder='Describe The Expence' />
+          <input type='text' placeholder='Describe The Expence' name='desciption'  value={desciptionValue}  onChange={descriptionHandler}/>
           <label> Category</label>
-          <select>
+          <select  value={categoryValue} onChange={categoryHandler}>
             <option value="Health">Health</option>
             <option value="Electronics">Electronics</option>
             <option value="Travel">Travel</option>
@@ -22,14 +77,14 @@ const CreateExpence = () => {
             <option value="Others">Others</option>
           </select>
           <label>Date Of Expence</label>
-          <input type='datetime-local' placeholder='Date Of Expence' />
+          <input type='date' placeholder='Date Of Expence' name='dateOfExpence'  value={dateOfExpenceValue}   onChange={dateOfExpenceHandler}/>
           <label>Expence Amount</label>
-          <input type='number' placeholder='Expence Amount In Inr' />
+          <input type='number' placeholder='Expence Amount In Inr' name='expenenceAmout'  value={expenenceAmoutValue}  onChange={expenenceAmoutHandler} />
           
         </form>
         <div className='cancel-create'>
-        <button className='cancel'>Cancel</button>
-          <button className='Create'>Create</button>
+        <button className='cancel' onClick={NaviagetToExpence}>Cancel</button>
+          <button className='Create' onClick={handleSubmit}>Create</button>
           </div>
       </div>
     </div>
--- src/Expense/ViewExpence.js
+++ src/Expense/ViewExpence.js
@@ -1,11 +1,58 @@
 import React from 'react'
-
+import { useEffect,useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import '../Expense/ViewExpence.css'
 const ViewExpence = () => {
+  const [fetcheData,setFechedData]=useState([])
+  const navigate=useNavigate()
+  useEffect(() => {
+    async function fetching() {
+      const response = await JSON.parse(localStorage.getItem("createExpenceData"));
+      setFechedData(response)
+      console.log(response)
+    }
+    fetching();
+  },[]);
+  
+    const NaviageToCreateExpence=()=>{
+      navigate('/CreateExpence')
+    }
+  
   return (
     <div>
-     <h1>Hello world</h1>
+      <div className='view-container'>
+        <h1>My Expence Manager</h1>
+        <input placeholder='Filter By Date Of Expence'/>
+        <input placeholder='Search Expence by Name'/>
+        <button onClick={NaviageToCreateExpence}>New Expence</button>
+      </div>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Category</th>
+        <th>Date of Expense</th>
+        <th>Description</th>
+        <th>Expense Amount</th>
+        <th>Updated at</th>
+        <th>Created by</th>
+       <th></th>
+      </tr>
+      {fetcheData.map((item) => {
+        const { nameValue, categoryValue, dateOfExpenceValue, desciptionValue, expenenceAmoutValue } = item;
+        return (
+          <tr>
+            <td>{nameValue}</td>
+            <td>{categoryValue}</td>
+            <td>{dateOfExpenceValue}</td>
+            <td>{desciptionValue}</td>
+            <td>{expenenceAmoutValue}</td>
+          </tr>
+        );
+      })}
+    </table>
     </div>
-  )
+  );
+  
 }
 
 export default ViewExpence
